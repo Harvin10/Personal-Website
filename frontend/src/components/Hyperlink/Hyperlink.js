5746905589e9a796996040bbb6c5ae74fac86075
harvin pratama putra <harvin.pratama@gmail.com>
2021-04-21 13:37:52 +0700

    component Hyperlink create component's style

--- frontend/src/components/Hyperlink/Hyperlink.js
+++ frontend/src/components/Hyperlink/Hyperlink.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Styles from './Hyperlink.module.css';
+import styles from './Hyperlink.module.css';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 function Hyperlink(props) {
     return (
         <Router>
-            <Link to={props.route.link}>{props.route.name}</Link>
+            <Link to={props.route.link} className={ styles.hyperlink }>{props.route.name}</Link>
         </Router>
     );
 }
